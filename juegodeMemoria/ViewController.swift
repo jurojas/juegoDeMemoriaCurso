e0904eb2bc98da00093ca88a47ec5fde611fa9e9
JUAN ANTONIO ROJAS HINOJOSA <juanantoniorojashinojosa@macbook-pro-de-juan.local>
2016-03-06 16:39:53 -0600

    Initial Commit

---
+++ juegodeMemoria/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  juegodeMemoria
+//
+//  Created by JUAN ANTONIO ROJAS HINOJOSA on 06/03/16.
+//  Copyright © 2016 JUAN ANTONIO ROJAS HINOJOSA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
